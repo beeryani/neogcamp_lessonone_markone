44f266d97d6964154318245b77022e82bab9a50f
pratyush <guptapratyush98@gmail.com>
2022-11-08 16:25:04 +0530

    chore: update highScore json

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const readlineSync = require('readline-sync');
 const fs = require('fs');
-//Ex13 & Ex14
 
 var highScore = fs.readFileSync('./highScore.json');
 var highScoreJson = JSON.parse(highScore)
 
 const questionOne = {
-  question: 'Who is my favorite superhero? ',
-  answer: 'Iron Man'
+    question: 'Who is my favorite superhero? ',
+    answer: 'Iron Man'
 }
 
 const questionTwo = {
-  question: 'What is my favorite dish? ',
-  answer: 'Beeryani'
+    question: 'What is my favorite dish? ',
+    answer: 'Beeryani'
 }
 
 const highScores = {}
@@ -21,29 +20,28 @@ let userScore = 0
 checkAnswerRewardUser(questionOne.question, questionOne.answer);
 checkAnswerRewardUser(questionTwo.question, questionTwo.answer);
 
-function checkAnswerRewardUser(questionStatement, correctAnswer){
-  var userAnswer = readlineSync.question(questionStatement)
-                                           
-  if (correctAnswer == userAnswer){
-    userScore++;
-    console.log('Hurray! You earned a point, your current score: ' + userScore);
-  }
-  else{
-    userScore--;
-    console.log('Sorry! Wrong Answer, your current score: ' + userScore)
-  }
+function checkAnswerRewardUser(questionStatement, correctAnswer) {
+    var userAnswer = readlineSync.question(questionStatement)
+
+    if (correctAnswer == userAnswer) {
+        userScore++;
+        console.log('Hurray! You earned a point, your current score: ' + userScore);
+    } else {
+        userScore--;
+        console.log('Sorry! Wrong Answer, your current score: ' + userScore)
+    }
 }
 
 highScoreJson['highScore'].push(userScore)
 var maxScore = Math.max.apply(null, highScoreJson['highScore']);
 
-if (maxScore == userScore){
-  console.log('Hey King, you dropped this! Send us a screenshot <3')
+if (maxScore == userScore) {
+    console.log('Hey King, you dropped this! Send us a screenshot <3')
 }
 
 var highScore = JSON.stringify(highScoreJson, null, 2);
 fs.writeFile('./highScore.json', highScore, err => {
     // error checking
-    if(err) throw err;
+    if (err) throw err;
     console.log("New data added");
-});
+});                                                 
